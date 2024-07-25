162aaabff711b0b568fc8c418509a94803be8b42
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-07-25 17:33:54 +0200

    Creating Role based Side bars & logout functionality

--- HotelWeb/src/app/app.component.ts
+++ HotelWeb/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserStorageService } from './auth/services/storage/user-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'HotelWeb';
+
+  isCustomerLoggedIn: boolean = UserStorageService.isCustomerLoggedIn();
+  isAdminLoggedIn: boolean = UserStorageService.isAdminLoggedIn();
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if(event.constructor.name === "NavigationEnd"){
+        this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
+        this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
+      }
+    })
+  }
+
+  logout() {
+    UserStorageService.signOut();
+    this.router.navigateByUrl("/");
+  }
 }
