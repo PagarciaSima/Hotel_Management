2369944d8fe2c65a2fa8e03deeb196dfb1eac0ea
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-07-26 06:33:57 +0200

    Creating room entity & restrict APIS by role

--- HotelServer/src/main/java/com/hotel/HotelServer/configs/WebSecurityConfiguration.java
+++ HotelServer/src/main/java/com/hotel/HotelServer/configs/WebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.hotel.HotelServer.configs;
 
+import com.hotel.HotelServer.enums.UserRole;
 import com.hotel.HotelServer.services.jwt.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +12,10 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,12 +23,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfiguration {
 
     private final UserService userService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
-                        request -> request.requestMatchers("/api/auth/**").permitAll());
+                        request -> request.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/api/admin/**").hasAnyAuthority(UserRole.ADMIN.name())
+                                .requestMatchers("/api/customer/**").hasAnyAuthority(UserRole.CUSTOMER.name())
+                                .anyRequest().authenticated())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider()).addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
