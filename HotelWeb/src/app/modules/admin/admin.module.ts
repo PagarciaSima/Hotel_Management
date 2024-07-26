bdf549a03b3931fa59b9ff68618c7512fc9e8245
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-07-26 17:04:43 +0200

    Creating Post Room page with reactive form

--- HotelWeb/src/app/modules/admin/admin.module.ts
+++ HotelWeb/src/app/modules/admin/admin.module.ts
@@ -4,16 +4,22 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PostRoomComponent } from './components/post-room/post-room.component';
+import { DemoNgZorroAntdModuleTsModule } from 'src/app/demo-ng-zorro-antd-module.ts.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    DashboardComponent
+    DashboardComponent,
+    PostRoomComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    DemoNgZorroAntdModuleTsModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
