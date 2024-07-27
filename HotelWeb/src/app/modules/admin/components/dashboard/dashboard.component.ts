41f0842342fcb95bbaba50752d6ab8013de93822
CORP\pagarcia <pablo.garcia-simavillal@capgemini.com>
2024-07-27 19:49:46 +0200

    Calling get all rooms api in angular

--- HotelWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
+++ HotelWeb/src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../admin-services/admin.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  currentPage = 1;
+  rooms = [];
+
+  constructor(
+    private adminService: AdminService,
+    private message: NzMessageService
+  ){}
+
+  ngOnInit(): void {
+    this.getRooms();
+  }
+
+  getRooms(){
+    this.adminService.getRooms(this.currentPage -1).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.rooms = res;
+      }
+    })
+  }
 }
